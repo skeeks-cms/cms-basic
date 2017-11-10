6b7b02fe252b8b640ab1994f4a1dc1ca241ad134
Саша <semenov@skeeks.com>
2017-11-10 15:17:24 +0300

    init

---
+++ src/config/common.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link https://skeeks.com/
+ * @copyright (c) 2010 SkeekS
+ * @date 10.11.2017
+ */
+return [
+
+    'aliases'     => [
+        '@common'   => '@root/common',
+        '@frontend' => '@root/frontend',
+        '@console'  => '@root/console',
+    ],
+
+];
---
+++ src/config/web.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link https://skeeks.com/
+ * @copyright (c) 2010 SkeekS
+ * @date 10.11.2017
+ */
+return [
+
+];
